38a06840acb953d40b26a1a835a07a0ee3042427
Josef Bauer <bauerjoe@gmx.de>
2017-03-12 19:19:01 +0100

    Added rocket and moon graphics

--- src/main/kotlin/de/pratho/praktikum/Display.kt
+++ src/main/kotlin/de/pratho/praktikum/Display.kt
@@ -1,5 +1,37 @@
 package de.pratho.praktikum
 
+import java.awt.Image
+import javax.imageio.ImageIO
+import javax.swing.ImageIcon
+import javax.swing.JFrame
+import javax.swing.JLabel
+
+object Display : JFrame("Gravity Simulator") {
+    var moonLabel = JLabel()
+    var rocketLabel = JLabel()
+
+    fun start() {
+        setSize(1000, 600)
+        loadGraphics()
+        defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+        isVisible = true
+    }
+
+    private fun loadGraphics() {
+        var imageStream = Display::class.java.getResourceAsStream("/moon.jpg")
+        val moonImage = ImageIO.read(imageStream).getScaledInstance(width, height, Image.SCALE_SMOOTH)
+        moonLabel = JLabel(ImageIcon(moonImage))
+        imageStream = Display::class.java.getResourceAsStream("/rocket.jpg")
+        val rocketImage = ImageIO.read(imageStream)
+        rocketLabel = JLabel(ImageIcon(rocketImage))
+        moonLabel.add(rocketLabel)
+        contentPane.add(moonLabel)
+    }
+
+    fun moveDown(distance: String) {
+    }
+}
+
 fun main(args: Array<String>) {
-    System.out.println("Läuft ...")
+    Display.start()
 }
